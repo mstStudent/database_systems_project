3cd897d49d1f0355a512231205a00735535a29ab
mstStudent <jpty9@mst.edu>
2015-09-30 15:30:14 -0500

    init

---
+++ js/index.js
@@ -0,0 +1,75 @@
+/*
+
+Schemas of the underlying relations;
+a. Sailors(sid:integer, sname:string, rating:integer, age:real)
+b. Boats(bid:integer, bname:string, color:string)
+c. Reserves(sid:integer, bid:integer, day:date)
+
+SELECT S.sname
+FROM Sailors AS S, Reserves AS R
+WHERE S.sid=R.sid AND R.bid=103
+
+SELECT S.sname
+FROM Sailors AS S, Reserves AS R, Boats AS B
+WHERE S.sid=R.sid AND R.bid=B.bid AND B.color=’red’
+
+SELECT sname
+FROM Sailors, Boats, Reserves
+WHERE Sailors.sid=Reserves.sid AND Reserves.bid=Boats.bid AND Boats.color=’red’
+UNION
+SELECT sname
+FROM Sailors, Boats, Reserves
+WHERE Sailors.sid=Reserves.sid AND Reserves.bid=Boats.bid AND Boats.color=’green’
+
+SELECT S.sname
+FROM Sailors AS S, Reserves AS R
+WHERE R.sid = S.sid AND R.bid = 100 AND S.rating > 5 AND R.day = ‘8/9/09’
+
+SELECT sname
+FROM Sailors, Boats, Reserves
+WHERE Sailors.sid=Reserves.sid AND Reserves.bid=Boats.bid AND Boats.color=’red’
+INTERSECT
+SELECT sname
+FROM Sailors, Boats, Reserves
+WHERE Sailors.sid=Reserves.sid AND Reserves.bid=Boats.bid AND Boats.color=’green’
+
+SELECT S.sid
+FROM Sailors AS S, Reserves AS R, Boats AS B
+WHERE S.sid=R.sid AND R.bid=B.bid AND B.color=‘red’
+EXCEPT
+SELECT S2.sid
+FROM Sailors AS S2, Reserves AS R2, Boats AS B2
+WHERE S2.sid=R2.sid AND R2.bid=B2.bid AND B.2color=‘green’
+
+SELECT S.sname
+FROM Sailors AS S
+WHERE S.sid IN ( SELECT R.sid
+                 FROM Reserve AS R
+                 WHERE R.bid = 103)
+
+SELECT S.sname
+FROM Sailors AS S
+WHERE S.sid IN ((SELECT R.sid
+		FROM Reserve AS R, Boats AS B
+		WHERE R.bid = B.bid AND B.color = ‘red’)
+		INTERSECT
+		(SELECT R2.sid
+		FROM Reserve AS R2, Boats AS B2
+		WHERE R2.bid = B2.bid AND B2.color = ‘green’))
+
+*/
+
+var startParse = function(){ 
+    var re = parseSQL($("#sqlQuery").val());
+    console.log(re);
+    $("#sqlResults").val(JSON.stringify(re));
+}
+
+var parseSQL = function (string){
+   re = null;
+   sqliteParser(string, function(error, results){
+      if(error){re = error;console.log(error);}
+      else{ re = results;}
+    });
+   return re;
+}
